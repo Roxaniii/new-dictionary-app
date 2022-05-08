d4796aaae79b1fff3b0645f5b4b2f925a1de133c
Roxaniii <92252445+Roxaniii@users.noreply.github.com>
2022-05-08 15:33:39 +0200

    add css styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header>
+          <h1 className="hero">Roxane's Fantastic Dictionary</h1>
+        </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>Coded by Roxane Kulenkamp</small>
